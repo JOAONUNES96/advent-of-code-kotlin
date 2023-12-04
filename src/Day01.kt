3969746da16de399e89830ed6499bba2c3f51d4c
Joao Nunes <Joao.Nunes@flutter.com>
2023-12-04 15:01:13 +0000

    Solved day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,61 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val digits = mutableListOf<Int>()
+        var firstDigit: String = "0"
+        var secondDigit: String = "0"
+
+        for (line in input) {
+            for (char in line) {
+                if (char.isDigit()) {
+                    if (firstDigit == "0") {
+                        firstDigit = char.toString()
+                    } else {
+                        secondDigit = char.toString()
+                    }
+                }
+            }
+            if (secondDigit == "0") {
+                secondDigit = firstDigit
+            }
+            digits.add((firstDigit + secondDigit).toInt())
+            firstDigit = "0"
+            secondDigit = "0"
+        }
+        println(digits)
+        return digits.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = mutableListOf<Int>()
+        var firstDigit: String = "0"
+        var secondDigit: String = "0"
+
+        for (line in input) {
+            for (char in line) {
+                if (char.isDigit()) {
+                    if (firstDigit == "0") {
+                        firstDigit = char.toString()
+                    } else {
+                        secondDigit = char.toString()
+                    }
+                }
+            }
+            if (secondDigit == "0") {
+                secondDigit = firstDigit
+            }
+            digits.add((firstDigit + secondDigit).toInt())
+            firstDigit = "0"
+            secondDigit = "0"
+        }
+        println(digits)
+        return digits.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+     val input = readInput("Day01")
+     part2(input).println()
 }
